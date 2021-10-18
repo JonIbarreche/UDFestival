2887e91fdd23a0b37e1ac518bab4746657733df8
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-10-18 10:05:17 +0200

    Probando Funcionalidad

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,10 +1,21 @@
 package gui;
 
+import java.awt.EventQueue;
+import gui.ExampleWindow;
+
 public class MainWindow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ExampleWindow frame = new ExampleWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }

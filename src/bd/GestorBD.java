54f171028603a1609a32ebe15b9159fe9d791414
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-10-25 11:44:10 +0200

    insertar usuario en la BD

--- src/bd/GestorBD.java
+++ src/bd/GestorBD.java
@@ -1,13 +1,19 @@
 package bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.apache.log4j.*;
+
+import jdo.Usuario;
+
+
 
 public class GestorBD {
 	
 	protected static Connection conn = null;
-	private static final Logger logger = LogManager.getLogger(BDException.class);
+	private PreparedStatement pStatement = null;
+	private static final Logger logger = LogManager.getLogger(GestorBD.class);
 	// Conectar BD
 	public static void cargarConectarDriver() {
 		try {
@@ -33,5 +39,26 @@ public class GestorBD {
 			throw new BDException("No se ha podido cerrar la conexion con la BD.", e);
 		}
 	}
+	
+	//REGISTRAR EN LA BD UN CLIENTE NUEVO
+		public void insertaUsuario(Usuario u) {
+			try {			
+				pStatement = conn.prepareStatement("INSERT INTO usuario VALUES (default,?,?,?,?,?)");
+				
+				pStatement.setString(1, u.getNombre());
+				pStatement.setString(2, u.getNickname());
+				pStatement.setString(3, u.getMail());
+				pStatement.setString(4, u.getPassword());
+				pStatement.setInt(5, u.getPhoneNumber());
+
+
+				//...
+				pStatement.executeUpdate();
+				conn.close();
+			} catch (SQLException e) {
+				logger.warn(e.getMessage());
+			} 	
+		}
+	
 	
 }

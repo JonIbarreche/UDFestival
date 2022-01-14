d1a91733370269d77188fce7b7cc1bc4937186b3
usuario <usuario@192.168.1.130>
2022-01-14 01:07:34 +0100

    Avances tabla conciertos

--- src/gui/ConsultaConciertosWindow.java
+++ src/gui/ConsultaConciertosWindow.java
@@ -1,19 +1,32 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
+import bd.GestorBD;
+
+
+
 
 
 public class ConsultaConciertosWindow extends JInternalFrame{
-	private JPanel contentPane;
-	private JTable tabla;
+	private JPanel contentPane, panelTabla;
+	private JTable tablaConciertos;
+	private DefaultTableModel modeloConciertos;
+	
+	
 	
 	public ConsultaConciertosWindow() {
+		tablaConciertos = new JTable();
+		add(new JScrollPane(tablaConciertos), BorderLayout.CENTER);
+		modeloConciertos = new DefaultTableModel();
+		
 		
 		setBounds(100, 100, 740, 500);
 		contentPane = new JPanel();
@@ -28,9 +41,27 @@ public class ConsultaConciertosWindow extends JInternalFrame{
 		lblNewLabel.setBounds(36, 13, 46, 40);
 		contentPane.add(lblNewLabel);
 		
-		DefaultTableModel modeloTabla = new DefaultTableModel();
+		tablaConciertos = new JTable();
+		add(new JScrollPane(tablaConciertos), BorderLayout.CENTER);
+		modeloConciertos = new DefaultTableModel();
+		panelTabla = new JPanel();
+		contentPane.add(panelTabla, BorderLayout.CENTER);
 		Object[] cabeceras = {"ID","ARTISTA","DESCRIPCION","CAPACIDAD","URL"};
-		modeloTabla.setColumnIdentifiers(cabeceras);
+		modeloConciertos.setColumnIdentifiers(cabeceras);
+		cargarModeloTabla();
+		tablaConciertos = new JTable(modeloConciertos);
+		panelTabla.add(tablaConciertos);
+		
+		private void cargarModeloTabla() {
+			GestorBD.cargarConectarDriver();
+			ArrayList<Concierto> a = GestorBD.obtenerDatosConcierto(con);
+			BD.closeBD(con);
+			for(Conncierto con: a) {
+				String [] fila = {con.getId(),con.getArtista(),con.getDescripcion(),String.valueOf(con.getCapacidad()),con.getUrl()};
+				modeloTabla.addRow(fila);
+			}
+		}
+	
 		/*
 		 * for (Concierto c : GestorBD.obtenerDatosConcierto()) {
 			String [] fila ==
